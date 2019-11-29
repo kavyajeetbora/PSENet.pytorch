516a86ea28984491c6634f1794c70be137f9ee45
Kavyajeet Bora <38955297+kavyajeetbora@users.noreply.github.com>
2019-11-29 10:32:25 +0530

    Update data_utils.py

--- dataset/data_utils.py
+++ dataset/data_utils.py
@@ -178,12 +178,12 @@ class MyDataset(data.Dataset):
                 params = line.strip().strip('\ufeff').strip('\xef\xbb\xbf').split(',')
                 try:
                     label = params[8]
-                    if len(label) == 0 or label == '##': # for ignore text only
+                    if len(label) == 0 or '##' in line: # for ignore text only
                         text_tags.append(True)
                     else: # where text is detected
                         text_tags.append(False)
                     # if label == '*' or label == '###':
-                    x1, x2, x3, x4, y1, y2, y3, y4 = list(map(float, params[:8]))
+                    x1, y1, x2, y2, x3, y3, x4, y4 = list(map(float, params[:8]))
                     boxes.append([[x1, y1], [x2, y2], [x3, y3], [x4, y4]])
                 except:
                     print('load label failed on {}'.format(label_path))
